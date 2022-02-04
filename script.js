c7b2e838b86265553d597733cbc7b3cb28fce06a
aaeas <easley.aaron89@gmail.com>
2022-02-04 09:38:42 -0600

    took off open locks in html and deleted enter button

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-var enterBtn = document.querySelector(".enterButton");
-
 var dt = new Date();
 document.getElementById('date-time').innerHTML = dt;
-
-enterBtn.addEventListener('click', function () {
-    enterBtn.setAttribute('class', 'hidden')
-});
 
 console.log("Connected");
 
