5e07890b578fec44aab306efffdd9c826cd8222e
aaeas <easley.aaron89@gmail.com>
2022-02-14 17:19:41 -0600

    update to html and console logged the pathway to save the information entered in the text

--- script.js
+++ script.js
@@ -14,16 +14,29 @@ var one = document.querySelector("#one");
 var two = document.querySelector("#two");
 var three = document.querySelector("#three");
 var four = document.querySelector("#four");
-var five = document.querySelector("#five ");
+var five = document.querySelector("#five");
 var fasFaLock = document.querySelectorAll(".fa-lock")
 
 // Create a function that fires on click of the button fas fa lock
-fasFaLock.addEventListener('click', function () {
-    console.log("click")
-});
 
+//create a forEach to loop through each element in the array and attach an event listener to that element
+
+fasFaLock.forEach(function (fasFaLock) {
+    fasFaLock.addEventListener('click', function (event) {
+        console.log(event.target);
+        console.log(event.target.parentNode);
+        console.log(event.target.parentNode.parentNode);
+        let formTableData = event.target.parentNode.parentNode.previousElementSibling;
+        console.log(formTableData.firstElementChild);
+        console.log(formTableData.firstElementChild.firstElementChild.value);
+        console.log("click")// event listener code in here)
+    });
+});
 // function handleButtonClick() {
     //here would go the code that uses the variables on line 8 to grab the user input and then do whatever your want with that data.
 //}
 
 // Set up an eventListener that fires the above function whenever a button is clicked
+
+// localStorage("KEY", value);
+// localStorage("12PM", "Bingo")
