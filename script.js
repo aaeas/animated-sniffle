e433cdf7ea729d5a1c803b57ba58056173e63ad6
aaeas <easley.aaron89@gmail.com>
2022-02-22 15:11:02 -0600

    updates to code

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var dt = new Date();
+//var localDate = dt.toLocalDateString();
 document.getElementById('date-time').innerHTML = dt;
 
 console.log("Connected");
@@ -25,18 +26,50 @@ fasFaLock.forEach(function (fasFaLock) {
     fasFaLock.addEventListener('click', function (event) {
         console.log(event.target);
         console.log(event.target.parentNode);
+        var button = event.target.parentNode;
         console.log(event.target.parentNode.parentNode);
         let formTableData = event.target.parentNode.parentNode.previousElementSibling;
         console.log(formTableData.firstElementChild);
-        console.log(formTableData.firstElementChild.firstElementChild.value);
-        console.log("click")// event listener code in here)
+        var tempVal = event.target.parentNode.parentNode.previousElementSibling.firstElementChild.firstElementChild.value;
+        console.log(tempVal);
+        var tempKey = event.target.parentNode.parentNode.parentNode.firstElementChild.textContent;
+        console.log(tempKey)// event listener code in here)
+
+        // - To retreive saved data
+        // localStorage.getItem("key");
+        // - To Save new or update data
+        // localStorage.setItem("key", "value");
+
+        localStorage.setItem(tempKey, tempVal);
     });
 });
 // function handleButtonClick() {
-    //here would go the code that uses the variables on line 8 to grab the user input and then do whatever your want with that data.
+//here would go the code that uses the variables on line 8 to grab the user input and then do whatever your want with that data.
 //}
 
 // Set up an eventListener that fires the above function whenever a button is clicked
 
-// localStorage("KEY", value);
+//localStorage("KEY", value);
 // localStorage("12PM", "Bingo")
+
+// See if we have DATA for respecitve KEY
+var nineAMData = localStorage.getItem("9AM");
+console.log(nineAMData);
+
+// IF we have DATA - update the DOM
+nine.value = nineAMData;
+
+//var storage = JSON.parse(localStorage.getItem('highscore'))
+//if (storage === null) {
+//    storage = []
+//}
+//var user = {
+//   name: input.value,
+//    currentScore: score
+//}
+//console.log(user)
+//storage.push(user)
+//localStorage.setItem('highscore', JSON.stringify(storage));
+// call for highscores
+//displayHighScore();
+//});
